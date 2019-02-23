f8c14cdb214475ea520673a385cf83c198b28cdd
Shubham Raichandel <shubhamraichandel@yahoo.com>
2019-02-23 17:30:38 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books308"
+  //Remote MongoDB deployment -> let 
+  "URI" :"mongodb://shubham:a123456@ds064278.mlab.com:64278/midterm-300950762"
+};
